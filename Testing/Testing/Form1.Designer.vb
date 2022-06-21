1235abd7b97f17b6a49004ba5b9ad764c6a8bb20
skeptal <85480277+skeptal@users.noreply.github.com>
2022-06-21 17:40:09 +0100

    Add files via upload

---
+++ Testing/Testing/Form1.Designer.vb
@@ -0,0 +1,74 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.CmdStart = New System.Windows.Forms.Button()
+        Me.CmdEnd = New System.Windows.Forms.Button()
+        Me.LSTDisplay = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'CmdStart
+        '
+        Me.CmdStart.Location = New System.Drawing.Point(73, 102)
+        Me.CmdStart.Name = "CmdStart"
+        Me.CmdStart.Size = New System.Drawing.Size(329, 104)
+        Me.CmdStart.TabIndex = 0
+        Me.CmdStart.Text = "Start"
+        Me.CmdStart.UseVisualStyleBackColor = True
+        '
+        'CmdEnd
+        '
+        Me.CmdEnd.Location = New System.Drawing.Point(73, 309)
+        Me.CmdEnd.Name = "CmdEnd"
+        Me.CmdEnd.Size = New System.Drawing.Size(329, 102)
+        Me.CmdEnd.TabIndex = 1
+        Me.CmdEnd.Text = "Stop"
+        Me.CmdEnd.UseVisualStyleBackColor = True
+        '
+        'LSTDisplay
+        '
+        Me.LSTDisplay.FormattingEnabled = True
+        Me.LSTDisplay.ItemHeight = 15
+        Me.LSTDisplay.Location = New System.Drawing.Point(455, 107)
+        Me.LSTDisplay.Name = "LSTDisplay"
+        Me.LSTDisplay.Size = New System.Drawing.Size(1048, 559)
+        Me.LSTDisplay.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1849, 1200)
+        Me.Controls.Add(Me.LSTDisplay)
+        Me.Controls.Add(Me.CmdEnd)
+        Me.Controls.Add(Me.CmdStart)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents CmdStart As Button
+    Friend WithEvents CmdEnd As Button
+    Friend WithEvents LSTDisplay As ListBox
+End Class
---
+++ Testing/Testing/Form1.vb
@@ -0,0 +1,48 @@
+﻿Public Class Form1
+    Dim customernumb As Decimal
+    Dim balance As Single
+    Dim customername As String
+    Dim customernumbint As Integer
+    Dim customernumbcorrect As Integer
+    Private Sub CmdStart_Click(sender As Object, e As EventArgs) Handles CmdStart.Click
+        LSTDisplay.Items.Clear()
+        customernumb = InputBox("What's Your Customer Number?")
+        customernumbint = customernumb.ToString.Length 'finding if xour account number is already long enough to be accepted first try'
+        If customernumbint = 10 Then
+            LSTDisplay.Items.Add("Welcome to The Bank. Your Account Number Is Valid.")
+        Else
+            MsgBox("You Have Many Too Characters Or Too Little Characters, The Maximum Is 10 And The Minimum is 10.")
+            Do Until customernumbint = 10
+                customernumb = InputBox("What's Your Customer Number?") 'if it isn't you'll get a reapated question to give acustomer number and answer which sazs it too large or too small' 
+                customernumbint = customernumb.ToString.Length
+                MsgBox("You Have Many Too Characters Or Too Little Characters, The Maximum Is 10 And The Minimum is 10.")
+                LSTDisplay.Items.Clear()
+            Loop
+        End If
+        customernumbcorrect = MsgBox("Is This Customer Number Correct? Customer Number Entered:" & customernumb, vbQuestion + vbYesNo + vbDefaultButton2, "Is This Correct?") 'asking weather or not the provided number is correct'
+        If customernumbcorrect = vbNo Then
+            Do Until customernumbcorrect = vbYes
+                LSTDisplay.Items.Clear()
+                MsgBox("Please Re-Input Your Account Number")
+                customernumb = InputBox("What's Your Customer Number?")
+                customernumbint = customernumb.ToString.Length
+                ElseIf customernumbint < 9 Or customernumbint > 11 Then
+                Do Until customernumbint = 10
+                    customernumb = InputBox("What's Your Customer Number?")
+                    customernumbint = customernumb.ToString.Length
+                    MsgBox("You Have Many Too Characters Or Too Little Characters, The Maximum Is 10 And The Minimum is 10.")
+                    LSTDisplay.Items.Clear()
+                    Else customernumbcorrect = MsgBox("Is This Customer Number Correct? Customer Number Entered:" & customernumb, vbQuestion + vbYesNo + vbDefaultButton2, "Is This Correct?")
+             Loop
+            Loop
+        End If
+        customernumbcorrect = MsgBox("Is This Customer Number Correct? Customer Number Entered:" & customernumb, vbQuestion + vbYesNo + vbDefaultButton2, "Is This Correct?")
+            If customernumbcorrect = vbYes Then
+
+        End If
+    End Sub
+
+    Private Sub CmdEnd_Click(sender As Object, e As EventArgs) Handles CmdEnd.Click
+        End
+    End Sub
+End Class
